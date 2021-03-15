dfa1eed6577d8f2d7248b7796120982da73ce8da
Yuri Oliveira <yurioliveirasilva1999@gmail.com>
2021-03-15 19:54:49 -0300

    Feat. Changing the structure of the Routes

---
+++ src/routes/products.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const routes = express.Router()
+const multer = require('../app/middlewares/multer')
+
+const Search = require('../app/controllers/SearchController')
+const Product = require('../app/controllers/ProductController')
+
+// Rota para Pesquisa
+routes.get('/products/search', Search.index)
+
+// Rotas para a Administração dos Produtos
+routes.get('/create', Product.create)                       // Redireciona para o formulário de cadastro
+routes.get('/:id', Product.show)                            // Exibe detalhes de um produto
+routes.get('/:id/edit', Product.edit)                       // Redireciona para o formulário de edição
+
+routes.post('/', multer.array("photos", 6), Product.post)   // Recebe os dados do Form, verifica os arq. do campo "Photos" e cadastra
+routes.put('/', multer.array("photos", 6), Product.put)     // Recebe os dados do Form, verifica os arq. do campo "Photos" e atualiza
+routes.delete('/', Product.delete)                          // Deleta um produto
+
+module.exports = routes
---
+++ src/routes/users.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const routes = express.Router()
+
+const Session = require('../app/controllers/SessionController')
+const User = require('../app/controllers/UserController')
+
+// // Rotas para Login / Logout
+// routes.get('/login', Session.loginForm)
+// routes.post('/login', Session.login)
+// routes.post('/logout', Session.logout)
+
+// // Rotas para Reset da Senha
+// routes.get('/forgot-password', Session.forgotForm)
+// routes.get('/password-reset', Session.resetForm)
+// routes.post('/forgot-password', Session.forgot)
+// routes.post('/password-reset', Session.reset)
+
+// // Rotas para Administração dos Usuários
+// routes.get('/register', User.registerForm)
+// routes.post('/register', User.post)
+
+// routes.get('/', User.show)
+// routes.put('/', User.update)
+// routes.delete('/', User.delete)
+
+module.exports = routes
