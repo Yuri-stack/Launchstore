a8e1b7715f3913d96ea5f737cebb59e522b85126
Yuri Oliveira <yurioliveirasilva1999@gmail.com>
2021-03-15 18:47:27 -0300

    Feat. Changing the database structure

--- diagram.sql
+++ diagram.sql
@@ -1,3 +1,4 @@
+DROP DATABASE IF EXISTS launchstoredb;
 CREATE DATABASE launchstoredb;
 
 CREATE TABLE "products" (
@@ -19,6 +20,10 @@ CREATE TABLE "categories" (
   "name" text NOT NULL
 );
 
+INSERT INTO categories (name) VALUES ('comida');
+INSERT INTO categories (name) VALUES ('eletrônicos');
+INSERT INTO categories (name) VALUES ('automóveis');
+
 CREATE TABLE "files" (
   "id" SERIAL PRIMARY KEY,
   "name" text,
@@ -26,40 +31,24 @@ CREATE TABLE "files" (
   "product_id" int
 );
 
+CREATE TABLE "users" (
+  "id" SERIAL PRIMARY KEY,
+  "name" text NOT NULL,
+  "email" text UNIQUE NOT NULL,
+  "password" text NOT NULL,
+  "cpf_cnpj" int UNIQUE NOT NULL,
+  "cep" text,
+  "address" text,
+  "created_at" timestamp DEFAULT (now()),
+  "updated_at" timestamp DEFAULT (now())
+);
+
+-- Constraints
 ALTER TABLE "products" ADD FOREIGN KEY ("category_id") REFERENCES "categories" ("id");
-
 ALTER TABLE "files" ADD FOREIGN KEY ("product_id") REFERENCES "products" ("id") ON DELETE CASCADE;
+ALTER TABLE "products" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
 
--- CREATE TABLE "products" (
---   "id" SERIAL PRIMARY KEY,
---   "category_id" int UNIQUE,
---   "user_id" int UNIQUE,
---   "name" text NOT NULL,
---   "description" text NOT NULL,
---   "old_price" int,
---   "price" int NOT NULL,
---   "quantity" int DEFAULT 0,
---   "status" int DEFAULT 1,
---   "created_at" timestamp DEFAULT (now()),
---   "updated_at" timestamp DEFAULT (now())
--- );
-
--- CREATE TABLE "categories" (
---   "id" SERIAL PRIMARY KEY,
---   "name" text NOT NULL
--- );
-
--- CREATE TABLE "files" (
---   "id" SERIAL PRIMARY KEY,
---   "name" text,
---   "path" text NOT NULL,
---   "product_id" int UNIQUE
--- );
-
--- ALTER TABLE "products" ADD FOREIGN KEY ("category_id") REFERENCES "categories" ("id");
-
--- ALTER TABLE "files" ADD FOREIGN KEY ("product_id") REFERENCES "products" ("id");
-
+-- Functions
 CREATE FUNCTION trigger_set_timestamp()
 RETURNS TRIGGER AS $$
 BEGIN
@@ -68,7 +57,13 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+-- Triggers
 CREATE TRIGGER set_timestamp
 BEFORE UPDATE ON products
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
+
+CREATE TRIGGER set_timestamp
+BEFORE UPDATE ON users
 FOR EACH ROW
 EXECUTE PROCEDURE trigger_set_timestamp();
