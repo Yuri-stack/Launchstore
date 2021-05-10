77b06d29c9b29e5933b5ba13cf3b1d95a8c1954e
Yuri Oliveira <yurioliveirasilva1999@gmail.com>
2021-05-10 18:43:20 -0300

    Feat. User Seeds with Faker

---
+++ seed.js
@@ -0,0 +1,29 @@
+const User = require('./src/app/models/User')
+
+const { hash } = require('bcryptjs')
+
+const faker = require('faker')
+
+let usersIds = []
+
+async function createUsers(){
+    const users = []
+    const password = await hash('1111', 8)
+
+    while(users.length < 3){
+        users.push({
+            name: faker.name.findName(),
+            email: faker.internet.email(),
+            password,
+            cpf_cnpj: faker.random.number(99999999),
+            cep: faker.random.number(9999999999),
+            address: faker.address.streetName()
+        })
+    }
+
+    const usersPromise = users.map(user => User.create(user))
+
+    usersIds = await Promise.all(usersPromise)
+}
+
+createUsers()
