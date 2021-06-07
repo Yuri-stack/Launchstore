341fdc8f15e7ccf29bd045891a815aa4e704f6d7
Yuri Oliveira <yurioliveirasilva1999@gmail.com>
2021-06-07 20:41:16 -0300

    Feat. Erasing data from Mailer.js

--- src/lib/mailer.js
+++ src/lib/mailer.js
@@ -4,7 +4,7 @@ module.exports = nodemailer.createTransport({
     host: "smtp.mailtrap.io",
     port: 2525,
     auth: {
-      user: "bda6d591486271",
-      pass: "16b77251d0807a"
+      user: "",
+      pass: ""
     }
   });
